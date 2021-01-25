80695f5a05078aa059d11b20ba6cbcb0f53cdfcd
Roman <76904779+Mr-R0m4n@users.noreply.github.com>
2021-01-25 01:55:27 +0100

    Update dezimal_in_dual.py

--- dezimal_in_dual.py
+++ dezimal_in_dual.py
@@ -1,23 +1,38 @@
 
 #Umrechnung vom Dezimal- in das Dualsystem
 
+loop = True
 
-zahl = 0
-zwischenergebnis = 0
-dual = ""
-eingabe = False
-
-while eingabe == False:
-    try:
-        zahl = int(input("Geben Sie den Wert als ganze Zahl ein, der umgerechnet werden soll: "))
-    except ValueError:
-        print("Die Eingabe war nicht gültig")
+while loop == True:
+    
+    zahl = 0
+    dual = ""
+    eingabe = False
+    
+    while eingabe == False:
+        try:
+            zahl = int(input("Geben Sie den Wert als ganze Zahl ein, der umgerechnet werden soll: "))
+        except ValueError:
+            print("Die Eingabe war nicht gültig")
+        else:
+            eingabe = True
+    
+    while zahl != 0:
+        dual = str(zahl % 2) + dual
+        zahl = zahl // 2
+    print(dual)
+    
+    print()
+    print("Möchten Sie das Programm Wiederholen? ")
+    print("1 = Ja // 2 = nein")
+    redo = int(input("Ihre Auswahl? "))
+    print()
+    if redo == 1:
+        loop = True
+    elif redo == 2:
+        loop = False
     else:
-        eingabe = True
-
-while zahl != 0:
-    dual = str(zahl % 2) + dual
-    zahl = zahl // 2
-print(dual)
+        print("Ihre Eingabe ist ungültig!")
+                  
 
 
